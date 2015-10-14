3f302db3d0f580726e335904d888ead050e057fd
Kevin Zheng <sunday0303@gmail.com>
2015-10-14 18:44:02 -0400

    Initialize the floodfill algorithm

--- floodfill-api/exercises/floodfill/main.js
+++ floodfill-api/exercises/floodfill/main.js
@@ -1,12 +1,19 @@
 var main = function(ex) {
-    window.exdata = ex.data;
+    window.ex = ex;
 
+    var UP;
+    var DOWN;
+    var LEFT;
+    var RIGHT;
+    var OFF_BOARD;
+    var FILLED;
+
+    /* Initialize our model fields */
     var model = {};
     var initModel = function() {
-        if (typeof ex.data.model != 'undefined') {
+        if (typeof ex.data.model !== 'undefined') {
             model = ex.data.model;
         } else {
-            /* Initialize our model fields */
             model.rows = 5;
             model.cols = 5;
             var initBoard = function(rows, cols) {
@@ -20,7 +27,9 @@ var main = function(ex) {
                             row.push({
                                 visited: false,
                                 depth: null,
-                                direction: null
+                                direction: null,
+                                row: i,
+                                col: j   
                             });
                         }
                     }
@@ -29,12 +38,76 @@ var main = function(ex) {
                 return board;
             };
             model.board = initBoard(model.rows, model.cols);
+            /* Directions */
+            model.UP = 'u';
+            model.DOWN = 'd';
+            model.RIGHT = 'r';
+            model.LEFT = 'l';
+            UP = model.UP;
+            DOWN = model.DOWN;
+            RIGHT = model.RIGHT;
+            LEFT = model.LEFT;
+            model.dirOrder = [UP, RIGHT, DOWN, LEFT];
+            /* Return statuses */
+            model.OFF_BOARD = 'ob';
+            model.FILLED = 'f';
+            OFF_BOARD = model.OFF_BOARD;
+            FILLED = model.FILLED;
+            /* Save the state */
             ex.data.model = model;
         }
     };
 
     initModel();
+    
+    /* The floodfill algorithm */
 
+    var ff = {
+        nextStack: [],
+        prevStack: [],
+        init: function(row, col) {
+            ff.nextStack.push(model.board[row][col]);
+        },
+        next: function() {
+            var cur = ff.nextStack.pop();
+            var row = cur.row;
+            var col = cur.col;
+            if (row < 0 || row >= model.cols || col < 0 || col > model.cols) {
+                return OFF_BOARD;
+            }
+            if (model.board[row][col].visited) {
+                return FILLED;
+            }
+            for (var i = 0; i < model.dirOrder.length; i++) {
+                var dir = model.dirOrder[i];
+                var next;
+                switch (dir) {
+                    case UP:
+                        next.row = row-1;
+                        next.col = col;
+                        break;
+                    case RIGHT:
+                        next.row = row;
+                        next.col = col+1;
+                        break;
+                    case DOWN:
+                        next.row = row+1;
+                        next.col = col;
+                        break;
+                    case LEFT:
+                        next.row = row;
+                        next.col = col-1;
+                        break;
+                }
+                next.direction = dir;
+                ff.nextStack.push(next);
+            }
+            cur.visited = true;
+            ff.prevStack.push(cur);
+        },
+    };
+
+    ff.init(); 
    
 };
 
