aaebc4a59149fa9908168fd32b67b97e3a23f758
Jonathan Merrin <Jonmerrin@gmail.com>
2015-10-14 18:49:15 -0400

    Initial Commit for draw

--- floodfill-api/exercises/floodfill/main.js
+++ floodfill-api/exercises/floodfill/main.js
@@ -1,6 +1,6 @@
 var main = function(ex) {
     window.exdata = ex.data;
-
+    window.ex = ex
     var model = {};
     var initModel = function() {
         if (typeof ex.data.model != 'undefined') {
@@ -32,9 +32,47 @@ var main = function(ex) {
             ex.data.model = model;
         }
     };
-
     initModel();
 
-   
+    var drawArrow = function(start, end){
+        ex.graphics.ctx.fillStyle = "black";
+        ex.graphics.ctx.moveTo(start);
+        ex.graphics.ctx.lineTo(end);
+        ex.graphics.ctx.stroke();
+
+    }
+
+    var drawGrid = function(){
+        var width = ex.width()/2;
+        var height = ex.height()/2;
+        var margin = 20
+        for (var row = 0; row < model.rows; row++) {
+            for (var col = 0; col < model.cols; col++) {
+                if (model.board[row][col] == null) {
+                    ex.graphics.fillStyle = "black";
+                    ex.graphics.ctx.fillRect(col*width/model.cols + margin,
+                                               row*height/model.rows + margin,
+                                                width/model.cols,
+                                                height/model.rows);  
+                } else {
+                    ex.graphics.fillStyle = "green";
+                    if (model.board[row][col].visited) {
+                        ex.graphics.ctx.fillRect(col*width/model.cols + margin,
+                                                row*height/model.rows + margin,
+                                                width/model.cols,
+                                                height/model.rows); 
+
+                    } else {
+                    ex.graphics.ctx.strokeRect(col*width/model.cols + margin,
+                                               row*height/model.rows + margin,
+                                                width/model.cols,
+                                                height/model.rows);
+                    };
+                };
+
+            };
+        };
+    };
+    drawGrid();
 };
 
