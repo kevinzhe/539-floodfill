757de597c610c6f01ceb0288320c3d003f3fba7a
Kevin Zheng <sunday0303@gmail.com>
2015-10-14 20:48:50 -0400

    Fully implement ff.next function

--- floodfill-api/exercises/floodfill/main.js
+++ floodfill-api/exercises/floodfill/main.js
@@ -1,86 +1,84 @@
 var main = function(ex) {
     window.ex = ex;
 
-    var UP;
-    var DOWN;
-    var LEFT;
-    var RIGHT;
-    var OFF_BOARD;
-    var FILLED;
+    /* Directions */
+    var UP      = 'up';
+    var DOWN    = 'down';
+    var RIGHT   = 'right';
+    var LEFT    = 'left';
+
+    /* Return statuses */
+    var OFF_BOARD   = 'off';
+    var FILLED      = 'fill';
+    var BLOCKED     = 'block';
+    var MOVED       = 'move';
+    var DONE        = 'done';
 
     /* Initialize our model fields */
     var model = {};
     var initModel = function() {
         if (typeof ex.data.model !== 'undefined') {
             model = ex.data.model;
-        } else {
-            model.rows = 5;
-            model.cols = 5;
-            var initBoard = function(rows, cols) {
-                var board = [];
-                for (var i = 0; i < rows; i++) {
-                    var row = []
-                    for (var j = 0; j < cols; j++) {
-                        if (Math.random() < 0.1) {
-                            row.push(null);
-                        } else {
-                            row.push({
-                                visited: false,
-                                depth: null,
-                                direction: null,
-                                row: i,
-                                col: j   
-                            });
-                        }
-                    }
-                    board.push(row);
-                }
-                return board;
-            };
-            model.board = initBoard(model.rows, model.cols);
-            /* Directions */
-            model.UP = 'u';
-            model.DOWN = 'd';
-            model.RIGHT = 'r';
-            model.LEFT = 'l';
-            UP = model.UP;
-            DOWN = model.DOWN;
-            RIGHT = model.RIGHT;
-            LEFT = model.LEFT;
-            model.dirOrder = [UP, RIGHT, DOWN, LEFT];
-            /* Return statuses */
-            model.OFF_BOARD = 'ob';
-            model.FILLED = 'f';
-            OFF_BOARD = model.OFF_BOARD;
-            FILLED = model.FILLED;
-            /* Save the state */
-            ex.data.model = model;
+            return;
         }
+        model.rows = 5;
+        model.cols = 5;
+        var initBoard = function(rows, cols) {
+            var board = [];
+            for (var i = 0; i < rows; i++) {
+                var row = []
+                for (var j = 0; j < cols; j++) {
+                    if (Math.random() < 0.1) {
+                        row.push(null);
+                    } else {
+                        row.push({
+                            visited: false,
+                            depth: null,
+                            direction: null,
+                            row: i,
+                            col: j   
+                        });
+                    }
+                }
+                board.push(row);
+            }
+            return board;
+        };
+        model.board = initBoard(model.rows, model.cols);
+        model.dirOrder = [UP, RIGHT, DOWN, LEFT];
+        ex.data.model = model;
     };
-
     initModel();
     
-    /* The floodfill algorithm */
-
+    /* The floodfill object */
     var ff = {
         nextStack: [],
         prevStack: [],
         init: function(row, col) {
-            ff.nextStack.push(model.board[row][col]);
+            ff.nextStack.push({ row: row, col: col });
         },
         next: function() {
+            if (ff.nextStack.length === 0) {
+                return DONE;
+            }
             var cur = ff.nextStack.pop();
             var row = cur.row;
             var col = cur.col;
-            if (row < 0 || row >= model.cols || col < 0 || col > model.cols) {
+            if (row < 0 || row >= model.cols || col < 0 || col >= model.cols) {
                 return OFF_BOARD;
+            }
+            if (model.board[row][col] === null) {
+                return BLOCKED;
             }
             if (model.board[row][col].visited) {
                 return FILLED;
             }
+            var curFrame = model.board[cur.row][cur.col];
+            curFrame.visited = true;
+            ff.prevStack.push(cur);
             for (var i = 0; i < model.dirOrder.length; i++) {
                 var dir = model.dirOrder[i];
-                var next;
+                var next = {};
                 switch (dir) {
                     case UP:
                         next.row = row-1;
@@ -102,12 +100,21 @@ var main = function(ex) {
                 next.direction = dir;
                 ff.nextStack.push(next);
             }
-            cur.visited = true;
-            ff.prevStack.push(cur);
+            return MOVED;
         },
     };
 
-    ff.init(); 
+    window.ff = ff;
+
+    window.printBoard = function() {
+        for (var i = 0; i < model.rows; i++) {
+            for (var j = 0; j < model.cols; j++) {
+                if (model.board[i][j] !== null) {
+                    console.log(model.board[i][j].visited);
+                }
+            }
+        }
+    }
    
 };
 
