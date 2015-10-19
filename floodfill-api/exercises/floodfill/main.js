7e6276777e590fae793b14df717a2d13d4b21772
Kevin Zheng <sunday0303@gmail.com>
2015-10-19 13:50:13 -0400

    Add per-step arrows

--- floodfill-api/exercises/floodfill/main.js
+++ floodfill-api/exercises/floodfill/main.js
@@ -19,8 +19,6 @@ var main = function(ex) {
     var DONE        = 'done';
 
 
-    var arrows  = [] //I couldn't find a better way to wipe the arrows.
-                    // I'll explain when we meet up.
     var margin = 20
     /* Initialize our model fields */
     var model = {};
@@ -42,7 +40,8 @@ var main = function(ex) {
                         row.push({
                             visited: false,
                             depth: 0,
-                            direction: null,
+                            fromDir: null,
+                            visitedDirs: [],
                             row: i,
                             col: j   
                         });
@@ -62,29 +61,57 @@ var main = function(ex) {
     var ff = {
         nextStack: [],
         prevStack: [],
+        callStack: [],
         initialRow: 0,
         initialCol: 0,
         curRow:     0,
         curCol:     0,
         init: function(row, col) {
-            ff.nextStack = []
-            ff.prevStack = []
-            ff.nextStack.push({ row: row, col: col, depth: 0 });
+            ff.nextStack = [];
+            ff.prevStack = [];
+            ff.nextStack.push({
+                row: row,
+                col: col,
+                depth: 0,
+                code: model.dirOrder,
+            });
             ff.initialRow = row; //I added these lines too
             ff.initialCol = col;
             ff.curRow = row;
             ff.curCol = col;
+            ff.callStack = [];
+            ff.prevCallStack = [];
             ff.next();
         },
         next: function() {
             if (ff.nextStack.length === 0) {
-                ff.curRow = ff.initialRow//I put this there
-                ff.curCol = ff.initialCol//I put this there
+                ff.curRow = ff.initialRow;//I put this there
+                ff.curCol = ff.initialCol;//I put this there
                 return DONE;
             }
             var cur = ff.nextStack.pop();
             var row = cur.row;
             var col = cur.col;
+            // Log the direction we came from
+            if (typeof cur.direction !== 'undefined') {
+                var dir = cur.direction;
+                var from;
+                switch (dir) {
+                    case UP:
+                        from = model.board[row+1][col];
+                        break;
+                    case RIGHT:
+                        from = model.board[row][col-1];
+                        break;
+                    case DOWN:
+                        from = model.board[row-1][col];
+                        break;
+                    case LEFT:
+                        from = model.board[row][col+1];
+                        break;
+                }
+                from.visitedDirs.push(dir);
+            }
             if (row < 0 || row >= model.cols || col < 0 || col >= model.cols) {
                 return OFF_BOARD;
             }
@@ -93,7 +120,7 @@ var main = function(ex) {
                 //ff.curCol = col; //yep, it's late.
                 //drawAll();
                 cur.success = false;
-                ff.prevStack.push(cur)
+                ff.prevStack.push(cur);
                 return BLOCKED;
             }
             if (model.board[row][col].visited) {
@@ -101,7 +128,7 @@ var main = function(ex) {
                 //ff.curCol = col; //So tired.
                 //drawAll();
                 cur.success = false;
-                ff.prevStack.push(cur)
+                ff.prevStack.push(cur);
                 return FILLED;
             }
             //I wrote this
@@ -110,8 +137,8 @@ var main = function(ex) {
             cur.success = true;
             //Up to here
             var curFrame = model.board[cur.row][cur.col];
-            curFrame.direction = cur.direction //I added this
-            curFrame.depth = cur.depth //And this
+            curFrame.fromDir = cur.direction; //I added this
+            curFrame.depth = cur.depth; //And this
             curFrame.visited = true;
             ff.prevStack.push(cur);
             for (var i = 0; i < model.dirOrder.length; i++) {
@@ -139,7 +166,6 @@ var main = function(ex) {
                 next.depth = cur.depth + 1; //Also this
                 ff.nextStack.push(next);
             }
-            drawAll();
             return MOVED;
         },
         reset: function(){
@@ -152,15 +178,11 @@ var main = function(ex) {
                     };
                 }
             };
-            for (var i = 0; i < arrows.length; i++) {
-                arrows[i].remove()
-            };
                 if(ex.data.meta.mode=="demo"){
                     ex.stopTimer(onTimer);
                     playButton.text("play")
                 }
                 ff.init(ff.initialRow, ff.initialCol);
-                drawAll();
         },
         stepBack: function(){
             if (ff.prevStack.length == 0){
@@ -172,61 +194,32 @@ var main = function(ex) {
                     ff.nextStack.pop();  
                 };
                 model.board[last.row][last.col].visited = false;
-                ff.curCol = last.col
-                ff.curRow = last.rows
+                ff.curCol = last.col;
+                ff.curRow = last.rows;
                 ff.nextStack.push(last);
             }
-            drawAll();
         }
     };
 
-    //Draws the arrows
-    var drawArrow = function(dir, startx, starty, width, height){
-                var img = "star.png";
-                var endx = startx;
-                var endy = starty;
-        switch(dir){
-            case UP:
-                endx = startx + width/4;
-                endy = starty+ 3*height/4;
-                img = "up.png";
-                break;
-            case DOWN:
-                endx = startx + width/4;
-                endy = starty - height/4;
-                img = "down.png";
-                break;
-            case LEFT:
-                endx = startx + 3*width/4;
-                endy = starty + height/4;
-                img = "left.png";
-                break;
-            case RIGHT:
-                endx = startx - width/4;
-                endy = starty + height/4;
-                img = "right.png";
-                break;
-            default:
-                endx = startx + width/4
-                endy = starty + height/4
-                break;
-            }
-            arrows.push(ex.createImage(endx,endy,img,{
-                width: (width/2).toString()+"px",
-                height: (height/2).toString()+"px"
-            }));
-
+    var drawFrame = function(frameDirs) {
+        for (var i = 0; i < frameDirs.length; i++) {
+            // something
         }
+    };
 
-    window.printBoard = function() {
-        for (var i = 0; i < model.rows; i++) {
-            for (var j = 0; j < model.cols; j++) {
-                if (model.board[i][j] !== null) {
-                    console.log(model.board[i][j].visited);
-                }
-            }
-        }
-    }
+    var drawArrow = function(context, fromx, fromy, tox, toy, color) {
+        var headlen = 10;   // length of head in pixels
+        var angle = Math.atan2(toy-fromy,tox-fromx);
+        context.beginPath();
+        context.strokeStyle = color;
+        context.lineWidth = 1;
+        context.moveTo(fromx, fromy);
+        context.lineTo(tox, toy);
+        context.lineTo(tox-headlen*Math.cos(angle-Math.PI/6),toy-headlen*Math.sin(angle-Math.PI/6));
+        context.moveTo(tox, toy);
+        context.lineTo(tox-headlen*Math.cos(angle+Math.PI/6),toy-headlen*Math.sin(angle+Math.PI/6));
+        context.stroke();
+    };
 
     //Draw the grid
     var drawGrid = function(){
@@ -235,61 +228,154 @@ var main = function(ex) {
         for (var row = 0; row < model.rows; row++) {
             for (var col = 0; col < model.cols; col++) {
                 var cell = model.board[row][col];
-                var xpos = col*width
-                var ypos = row*height
-                if (row == ff.curRow && col == ff.curCol) {
-                    ex.graphics.ctx.strokeStyle = "rgb(0,255,0)"
-                    ex.graphics.ctx.lineWidth = 5;
-                } else {
-                    ex.graphics.ctx.strokeStyle = "black"
-                    ex.graphics.ctx.lineWidth = 1
-                };
-                if (cell == null) {
+                var xpos = col*width;
+                var ypos = row*height;
+                ex.graphics.ctx.strokeStyle = "black";
+                ex.graphics.ctx.lineWidth = 1;
+                if (cell === null) {
+                    // Blocked cell
                     ex.graphics.ctx.fillStyle = "black";
                     ex.graphics.ctx.fillRect(xpos + margin,
                                             ypos + margin,
                                             width,
                                             height);
                 } else {
-                    ex.graphics.ctx.font = (ex.width()/35).toString()+"px Courier"
+                    ex.graphics.ctx.font = (ex.width()/35).toString()+"px Courier";
                     ex.graphics.ctx.fillStyle = "rgb("+
                         (250*cell.depth/(model.rows*model.cols/2)).toString()
                         +",0,"+
                         (255-250*cell.depth/(model.rows*model.cols)).toString()
                         +")";
-                                        if (cell.visited) {
+                    if (cell.visited) {
+                        // visited cell
                         ex.graphics.ctx.fillRect(xpos + margin,
                                             ypos + margin,
                                             width,
-                                            height); 
-                        drawArrow(cell.direction, xpos+margin, 
-                                ypos+margin, width, height)
+                                            height);
                         ex.graphics.ctx.strokeRect(xpos + margin,
                                                     ypos + margin,
                                                     width,
                                                     height);
-                        ex.graphics.ctx.fillStyle = "black";
-                        ex.graphics.ctx.textAlign = "end"
+                        ex.graphics.ctx.fillStyle = "white";
+                        ex.graphics.ctx.textAlign = "end";
                         ex.graphics.ctx.fillText(
                             model.board[row][col].depth.toString(),
                             xpos + margin + width,
-                            ypos + 2*margin)
+                            ypos + 2*margin);
                     } else {
+                        // empty cell
                         ex.graphics.ctx.strokeRect(xpos + margin,
                                                 ypos + margin,
                                                 width,
                                                 height);
-                    };
-                };
-            };
-        };
+                    }
+                    // go through the visited dirs, make them bold
+                    ex.graphics.ctx.beginPath();
+                    ex.graphics.ctx.lineWidth = 5;
+                    ex.graphics.ctx.strokeStyle = "white";
+                    for (var i = 0; i < cell.visitedDirs.length; i++) {
+                        var dir = cell.visitedDirs[i];
+                        var x0, y0, x1, y1;
+                        switch (dir) {
+                            case UP:
+                                x0 = xpos+margin+width/2;
+                                y0 = ypos+margin+height/8;
+                                x1 = x0;
+                                y1 = y0-height/8;
+                                // x0 = xpos+margin;
+                                // y0 = ypos+margin+2.5;
+                                // x1 = x0+width;
+                                // y1 = y0;
+                                break;
+                            case DOWN:
+                                x0 = xpos+margin+width/2;
+                                y0 = ypos+margin+height-height/8;
+                                x1 = x0;
+                                y1 = y0+height/8;
+                                // x0 = xpos+margin;
+                                // y0 = ypos+margin+height-2.5;
+                                // x1 = x0+width;
+                                // y1 = y0;
+                                break;
+                            case RIGHT:
+                                x0 = xpos+margin+width-width/8;
+                                y0 = ypos+margin+height/2;
+                                x1 = x0+width/8;
+                                y1 = y0;
+                                // x0 = xpos+margin+width-2.5;
+                                // y0 = ypos+margin;
+                                // x1 = x0;
+                                // y1 = y0+height;
+                                break;
+                            case LEFT:
+                                x0 = xpos+margin+width/8;
+                                y0 = ypos+margin+height/2;
+                                x1 = x0-width/8;
+                                y1 = y0;
+                                // x0 = xpos+margin+2.5;
+                                // y0 = ypos+margin;
+                                // x1 = x0;
+                                // y1 = y0+height;
+                                break;
+                        }
+                        // ex.graphics.ctx.moveTo(x0, y0);
+                        // ex.graphics.ctx.lineTo(x1, y1);
+                        // ex.graphics.ctx.stroke();
+                        drawArrow(ex.graphics.ctx, x0, y0, x1, y1, 'black');
+                    }
+                }
+            }
+        }
+        ex.graphics.ctx.beginPath();
+        ex.graphics.ctx.strokeStyle = "rgb(0,255,0)";
+        ex.graphics.ctx.lineWidth = 2;
+        ex.graphics.ctx.strokeRect(ff.curCol*width+margin,
+                               ff.curRow*height+margin,
+                               width,
+                               height); 
+        for (var row = 0; row < model.rows; row++) {
+            for (var col = 0; col < model.cols; col++) {
+                var cell = model.board[row][col];
+                if (cell !== null && cell.visited &&
+                    typeof cell.fromDir !== 'undefined' && cell.fromDir !== null) {
+                    var xpos = col*width;
+                    var ypos = row*height;
+                    switch (cell.fromDir) {
+                        case DOWN:
+                            x0 = xpos+margin+width/2;
+                            y0 = ypos+margin-height/4;
+                            x1 = x0;
+                            y1 = y0+height/2;
+                            break;
+                        case UP:
+                            x0 = xpos+margin+width/2;
+                            y0 = ypos+margin+height+height/4;
+                            x1 = x0;
+                            y1 = y0-height/2;
+                            break;
+                        case RIGHT:
+                            x0 = xpos+margin-width/4;
+                            y0 = ypos+margin+height/2;
+                            x1 = x0+width/2;
+                            y1 = y0;
+                            break;
+                        case LEFT:
+                            x0 = xpos+margin+width+width/4;
+                            y0 = ypos+margin+height/2;
+                            x1 = x0-width/2;
+                            y1 = y0;
+                            break;
+                    }
+                    drawArrow(ex.graphics.ctx, x0, y0, x1, y1, 'white');
+                }
+            }
+        }
     };
 
     //Check if the board is fully visited
     var checkFull = function(){
         for (var row = 0; row < model.rows; row++) {
             for(var col = 0; col < model.cols; col++){
-
                 if(model.board[row][col] != null){
                     if(model.board[row][col].visited == false){return false};
                 }
@@ -308,8 +394,9 @@ var main = function(ex) {
             height: "20px"
         }).on("click", function(){
             ff.next();
+            drawAll();
             ex.stopTimer(onTimer);
-            playButton.text("play")
+            playButton.text("play");
         });
             //Play and Pause button
         var playButton = ex.createButton(2*ex.width()/8+margin,
@@ -319,8 +406,9 @@ var main = function(ex) {
         })
         playButton.on("click", function(){
             if (playButton.text() == "play") {
-            onTimer = ex.onTimer(200,function () { 
+            onTimer = ex.onTimer(500,function () { 
                         ff.next();
+                        drawAll();
                     });
             playButton.text("pause");
             } else {
@@ -336,6 +424,7 @@ var main = function(ex) {
             height: "20px"
             }).on("click", function(){
                         ff.stepBack();
+                        drawAll();
             });
         } 
 
@@ -346,6 +435,7 @@ var main = function(ex) {
             height: "20px"
             }).on("click", function(){
                         ff.reset();
+                        drawAll();
             });
 
     ex.chromeElements.resetButton.on("click", function(){ff.reset();})
@@ -371,17 +461,11 @@ var main = function(ex) {
         }
     }
 
-
-
-
     var drawAll = function(){
         ex.graphics.ctx.clearRect(0,0,ex.width(),ex.height());
-        for (var i = 0; i < arrows.length; i++) {
-            arrows[i].remove()
-        };
         drawGrid();
         code.draw();
-    }
+    };
     code.init();
     ff.init(Math.floor(Math.random()*model.rows),Math.floor(Math.random()*model.cols)); 
     drawAll();
@@ -420,6 +504,7 @@ var main = function(ex) {
                     }
                 }
         }
+        drawAll();
 
     });
 
